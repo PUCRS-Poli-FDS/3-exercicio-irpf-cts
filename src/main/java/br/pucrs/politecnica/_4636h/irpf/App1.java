1bdb3a40d45d7a7219d5379739ac142e02fd5e46
dantefleschpereira <48799984+dantefleschpereira@users.noreply.github.com>
2019-11-05 19:39:10 -0300

    Inclusao DAO

--- src/main/java/br/pucrs/politecnica/_4636h/irpf/App1.java
+++ src/main/java/br/pucrs/politecnica/_4636h/irpf/App1.java
@@ -1,5 +1,8 @@
 package br.pucrs.politecnica._4636h.irpf;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.pucrs.politecnica._4636h.irpf.db.ContribuinteDaoJDBC;
 import br.pucrs.politecnica._4636h.irpf.db.DB;
 import br.pucrs.politecnica._4636h.irpf.model.Contribuinte;
@@ -12,19 +15,31 @@ public class App1 {
 		
 		ContribuinteDAO contribuinteDAO = new ContribuinteDaoJDBC(DB.getConnection());
 		/*
-		System.out.println("## Teste findByCPF ##");		
+		System.out.println("=== Teste 1: findByCPF ===");		
 		System.out.println(contribuinteDAO.findByCPF("75098814050"));
-		*/
 		
-		/*
-		System.out.println("## Teste insert ##");
+		System.out.println("=== Teste 2: insert ===");
 		Contribuinte contribuinte1 = new Contribuinte();
 		contribuinte1.setNome("Marco Polo");
 		contribuinte1.setCpf("50098814040");
 		contribuinte1.setIdade(45);
 		contribuinte1.setNumDependentes(2);		
 		contribuinteDAO.insert(contribuinte1);
+		
+		System.out.println("=== Teste 3: update ===");
+		Contribuinte contribuinte = contribuinteDAO.findByCPF("50098814040");
+		contribuinte.setIdade(70);
+		contribuinteDAO.update(contribuinte);		
+		
+		System.out.println("=== Test 4: findAll ===");
+		List<Contribuinte> lista = new ArrayList<>();
+		lista = contribuinteDAO.findaAll();
+		for(Contribuinte c : lista) {
+			System.out.println(c);
+		}
+		
 		*/
+
 	}
 
 }
--- src/main/java/br/pucrs/politecnica/_4636h/irpf/db/ContribuinteDaoJDBC.java
+++ src/main/java/br/pucrs/politecnica/_4636h/irpf/db/ContribuinteDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import br.pucrs.politecnica._4636h.irpf.model.Contribuinte;
@@ -20,11 +21,9 @@ public class ContribuinteDaoJDBC implements ContribuinteDAO {
 	@Override
 	public void insert(Contribuinte obj) {
 		try {
-			String insert = "INSERT INTO contribuintes "
-							+ "(nome, cpf, idade, num_dependentes) "
-							+ "VALUES "
-							+ "(?, ?, ?, ?) ";
-			
+			String insert = "INSERT INTO contribuintes " + "(nome, cpf, idade, num_dependentes) " + "VALUES "
+					+ "(?, ?, ?, ?) ";
+
 			conn = DB.getConnection();
 			PreparedStatement st = conn.prepareStatement(insert);
 			st.setString(1, obj.getNome());
@@ -32,22 +31,34 @@ public class ContribuinteDaoJDBC implements ContribuinteDAO {
 			st.setInt(3, obj.getIdade());
 			st.setInt(4, obj.getNumDependentes());
 			st.executeUpdate();
-		}catch (SQLException e) {
-			throw new DbException (e.getMessage());
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
 		}
 
 	}
 
 	@Override
 	public void update(Contribuinte obj) {
-		// TODO Auto-generated method stub
-
+		String sql = "UPDATE contribuintes SET " + "nome = ?, cpf = ?, idade = ?, num_dependentes = ? "
+				+ "WHERE cpf = ? ";
+		try {
+			conn = DB.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setString(1, obj.getNome());
+			st.setString(2, obj.getCpf());
+			st.setInt(3, obj.getIdade());
+			st.setInt(4, obj.getNumDependentes());
+			st.setString(5, obj.getCpf());
+			st.executeUpdate();
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
 	}
 
 	@Override
 	public void deleteByCpf(String cpf) {
-		// TODO Auto-generated method stub
 
+		
 	}
 
 	@Override
@@ -55,33 +66,48 @@ public class ContribuinteDaoJDBC implements ContribuinteDAO {
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		try {
-			st = conn.prepareStatement(
-					"SELECT * FROM contribuintes WHERE contribuintes.cpf = ?"
-					);
+			st = conn.prepareStatement("SELECT * FROM contribuintes WHERE contribuintes.cpf = ?");
 			st.setString(1, cpf);
 			rs = st.executeQuery();
-			if(rs.next()) {
+			if (rs.next()) {
 				Contribuinte contribuinte = new Contribuinte();
 				contribuinte.setNome(rs.getString("nome"));
 				contribuinte.setCpf(rs.getString("cpf"));
 				contribuinte.setIdade(rs.getInt("idade"));
-			//	contribuinte.setTotalRendimentos(rs.getCurrency("total_rendimentos"));
-			//	contribuinte.setContribuicaoPrevidenciaria(rs.getCurrency("contribuicao_prev"));
+				// contribuinte.setTotalRendimentos(rs.getCurrency("total_rendimentos"));
+				// contribuinte.setContribuicaoPrevidenciaria(rs.getCurrency("contribuicao_prev"));
 				contribuinte.setNumDependentes(rs.getInt("num_dependentes"));
 				return contribuinte;
 			}
 			return null;
-			
-		}catch (SQLException e) {
-			throw new DbException (e.getMessage());
+
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
 		}
-		
+
 	}
 
 	@Override
 	public List<Contribuinte> findaAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement("SELECT * FROM contribuintes ");
+			rs = st.executeQuery();
+			List<Contribuinte> contribuintes = new ArrayList<>();
+			while(rs.next()) {
+				Contribuinte contribuinte = new Contribuinte();
+				contribuinte.setNome(rs.getString("nome"));
+				contribuinte.setCpf(rs.getString("cpf"));
+				contribuinte.setIdade(rs.getInt("idade"));
+				contribuinte.setNumDependentes(rs.getInt("num_dependentes"));
+				contribuintes.add(contribuinte);
+			}
+			return contribuintes;
+			
+		}catch (SQLException e) {
+			throw new DbException (e.getMessage());
+		}
 	}
 
 }
