cf90d6ab52ed328193158666c992679dac91e427
Paulo Vianna Mu <paulomu@live.com>
2019-11-05 22:01:22 -0200

    Factory

--- src/main/java/br/pucrs/politecnica/_4636h/irpf/App.java
+++ src/main/java/br/pucrs/politecnica/_4636h/irpf/App.java
@@ -1,5 +1,8 @@
 package br.pucrs.politecnica._4636h.irpf;
 
+import br.pucrs.politecnica._4636h.irpf.application.CalculoIrpf;
+import br.pucrs.politecnica._4636h.irpf.application.CalculoIrpfFactory;
+import br.pucrs.politecnica._4636h.irpf.model.TipoCalculo;
 import br.pucrs.politecnica._4636h.irpf.view.Menu;
 
 public class App {
@@ -7,6 +10,8 @@ public class App {
     public static void main(String[] args) {
         Menu menu = new Menu();
         menu.exibir();
+
+        CalculoIrpf calculo = new CalculoIrpfFactory().getCalculo(TipoCalculo.COMPLETO);
     }
 
 }
---
+++ src/main/java/br/pucrs/politecnica/_4636h/irpf/model/TipoCalculo.java
@@ -0,0 +1,8 @@
+package br.pucrs.politecnica._4636h.irpf.model;
+
+public enum TipoCalculo {
+
+    COMPLETO,
+    SIMPLIFICADO;
+
+}
